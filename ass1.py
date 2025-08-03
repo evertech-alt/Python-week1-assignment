d4c39762234c106319c33fff1b10c512d91e76fb
evertech-alt <evanjeri009@gmail.com>
2025-08-03 14:19:18 +0300

    PLP python week 1

---
+++ ass1.py
@@ -0,0 +1,20 @@
+num1 = float(input("Enter the first number: "))
+
+operation = input("Enter an operation(+,-,*,/): ")
+
+num2 = float(input("Enter the second number: "))
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2 
+    else : result = "Cannot be divided by 0"
+else:
+    result = "Invalid"
+
+print(f"{num1} {operation} {num2} = {result}")
